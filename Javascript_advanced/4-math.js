266ae78d8e244972fa8fed0d076ff230856201aa
EveBaker <evebaker0162@gmail.com>
2023-11-15 16:55:38 -0600

    task 4  done

---
+++ Javascript_advanced/4-math.js
@@ -0,0 +1,27 @@
+function divideBy(firstNumber) {
+    return function(secondNumber){
+        return secondNumber / firstNumber;
+    };
+}
+
+function addBy(firstNumber) {
+    return function(secondNumber) {
+        return firstNumber + secondNumber;
+    };
+}
+
+var addBy100 = addBy(100);
+var addBy1000 = addBy(1000);
+var divideBy10 = divideBy(10);
+var divideBy100 = divideBy(100);
+
+/*
+things to know:
+Higher-Order Functions: Functions that return other functions.
+
+to test use:
+console.log(addBy100(20));  this Should display 120
+console.log(divideBy10(20)); this Should display 2
+console.log(divideBy100(200)); this Should display 2
+console.log(addBy1000(20));  this Should display 1020
+*/
