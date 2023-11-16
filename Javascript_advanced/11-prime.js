1eea61f34f041bcf4e85ca6ffb28de4fabef005f
EveBaker <evebaker0162@gmail.com>
2023-11-15 18:42:18 -0600

    task 11 done

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,26 @@
+function isPrime(number) {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return number > 1;
+}
+
+function countPrimeNumbers() {
+    let count = 0;
+    for (let number = 2; number <= 100; number++) {
+        if (isPrime(number)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const startTime = performance.now();
+for (let i = 0; i < 100; i++) {
+    setTimeout(countPrimeNumbers, 0);
+}
+const endTime = performance.now();
+
+console.log(`Execution time of setting up calculating prime numbers 100 times was ${endTime - startTime} milliseconds.`);
